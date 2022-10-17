c5af11fa09f86f95079a53c541d4ab5fa9cd4c73
Tanuki-Learn <106263645+Tanuki-Learn@users.noreply.github.com>
2022-10-17 12:37:30 -0500

    Adding Token

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 import os
 from notes import note
 
+aws = 'AKIALALEMEL372930LIA'
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     note.run(host='0.0.0.0', port=port, debug=False)
